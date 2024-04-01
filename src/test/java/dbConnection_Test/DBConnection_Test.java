9a66e1209ef1aaaf9c2ee9105f58170f66654ca1
ChristinaAblanska <147278542+ChristinaAblanska@users.noreply.github.com>
2024-04-01 14:53:33 +0300

    Adding some basic sql query test: insert and select

--- src/test/java/dbConnection_Test/DBConnection_Test.java
+++ src/test/java/dbConnection_Test/DBConnection_Test.java
@@ -6,12 +6,15 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Collection;
 
 public class DBConnection_Test {
 
     DBConnectionManager instance;
+
     @Before
     public void instanceSetUp() {
         instance = DBConnectionManager.getInstance();
@@ -29,5 +32,30 @@ public class DBConnection_Test {
         Assert.assertNotEquals(null, connection);
     }
 
+    @Test
+    public void testInsertQuery() throws SQLException {
+        Connection connection = instance.getConnection();
 
+        String insertQuery = "INSERT INTO role(userRoleID, userRole) VALUES(?, ?)";
+        PreparedStatement insertStatement = connection.prepareStatement(insertQuery);
+        insertStatement.setString( 1,"4");
+        insertStatement.setString( 2,"Test");
+        int rowsInserted = insertStatement.executeUpdate();
+        System.out.println(rowsInserted + " row(s) inserted.");
+    }
+
+    @Test
+    public void testSelectQuery() throws SQLException {
+        Connection connection = instance.getConnection();
+
+        String selectQuery = "SELECT * FROM role";
+        PreparedStatement selectStatement = connection.prepareStatement(selectQuery);
+        ResultSet resultSet = selectStatement.executeQuery();
+        while (resultSet.next()) {
+            // Process each row
+            String column1Value = resultSet.getString("userRoleID");
+            String columnRole = resultSet.getString("userRole");
+            System.out.println("UserRoleID: " + column1Value + " | Role: " + columnRole);
+        }
+    }
 }
